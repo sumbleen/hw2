49b8886b086a8b3b8efb310878c52cb77a147e65
sumbleen <42788130+sumbleen@users.noreply.github.com>
2018-10-01 14:42:44 -0400

    re-do hw 2

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### INSERT YOUR CODE BELOW ###
-
+dcm2bids -d /bind/raw/IBRAIN002 -o /bind/bids -p 002 -c /bind/scripts/dcm2bids.json
 
 ### DO NOT MODIFY THE LINES BELOW ###
 uname -a > info_host.txt
